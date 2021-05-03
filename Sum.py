279d2e97aa17ed92ef55a4119546c2a8851fc828
Ali <64139755+Altaier-byte@users.noreply.github.com>
2021-05-03 00:56:24 -0400

    First commit sum

---
+++ Sum.py
@@ -0,0 +1,5 @@
+first_Num = int(input("Enter the first number: "))
+second_Num = int(input("Enter the second number: "))
+third_Num = int(input("Enter the third number: "))
+sum = first_Num + second_Num + third_Num
+print("the sum of the numbers is: " + str(sum))
